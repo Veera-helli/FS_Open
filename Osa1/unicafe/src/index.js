17dbb61ac9070959aae486b9b2b018da7ca666e1
Veera <veera.helli@gmail.com>
2020-12-30 20:18:17 +0200

    1.7 done

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -12,17 +12,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
   
   return (
@@ -34,9 +38,14 @@ const App = () => {
       <br></br>
 
       <h2>Statistics</h2>
-      <p>good {good} </p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br /><br />
+        all {all} <br />
+        average {(good-bad)/all} <br />
+        positive {good/all} %<br />
+      </p>
     </div>
   )
 }

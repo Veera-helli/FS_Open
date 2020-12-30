1335cabf639e1ed5fcbd5c29462d6a6ac5b9678d
Veera <veera.helli@gmail.com>
2020-12-30 20:05:28 +0200

    1.6 done

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ onClick, text, color }) => (
+  <button onClick={onClick} style={{ backgroundColor: color }}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+  
   return (
     <div>
-      code here
+      <h2>Give feedback for Unicafe!</h2>
+      <Button onClick={handleGoodClick} text={"Good"} color={'green'} />
+      <Button onClick={handleNeutralClick} text={"Neutral"} color={'orange'}/>
+      <Button onClick={handleBadClick} text={"Bad"} color={'red'}/>
+      <br></br>
+
+      <h2>Statistics</h2>
+      <p>good {good} </p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }

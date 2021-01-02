65f23ca5606dff29df421680ef505d66ba4a26b4
Veera <veera.helli@gmail.com>
2021-01-02 18:34:13 +0200

    2.6 done

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+const Number = ({ person }) => {
+    return (    
+        <tr>
+            <td>{person.name}</td>
+        </tr> 
+    )
+}
+
+const App = () => {
+    const [ persons, setPersons] = useState([
+        { name: 'Arto Hellas'}
+    ]) 
+
+    const [ newName, setNewName ] = useState('')
+
+    const handleChange = (event) => {   
+        setNewName(event.target.value)  
+    }
+
+    const addName = (event) => {
+        event.preventDefault()
+        const nameObject = {
+          name: newName,
+        }
+      
+        setPersons(persons.concat(nameObject))
+        setNewName('')
+      }
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <table>
+          <tbody>
+            {persons.map(person =>           
+            <Number person={person} key={person.name} />        
+            )}
+          </tbody>
+      </table>
+    </>
+  )
+
+}
+
+export default App

971898fb716d50866bc35bb975ff22c1633509b8
Veera <veera.helli@gmail.com>
2021-01-02 19:23:26 +0200

    2.9 done

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -11,12 +11,13 @@ const Number = ({ person }) => {
 
 const App = () => {
     const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '045-12345678'}
+        { name: 'Arto Hellas', number: '045-12345678', showing: true}
     ]) 
 
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
-
+    const [ newFilter, setNewFilter] = useState('')
+    
     const handleChange = (event) => {   
         setNewName(event.target.value)  
     }
@@ -25,12 +26,32 @@ const App = () => {
         setNewNumber(event.target.value)  
     }
 
+    const handleFilterChange = (event) => {   
+        setNewFilter(event.target.value)
+        if (event.target.value !== ''){
+            for (let index = 0; index < persons.length; index++) {
+                const element = persons[index];
+                const name = element.name.toLowerCase()
+                if (!name.includes(event.target.value.toLowerCase())){
+                    element.showing = false
+                }
+                else{
+                    element.showing = true
+                }
+            }
+        }
+        else{
+            persons.forEach(person => person.showing = true)
+        }
+    }
+
     const addName = (event) => {
         event.preventDefault()
 
         const nameObject = {
           name: newName,
-          number: newNumber
+          number: newNumber, 
+          showing: true
         }
 
         if (persons.map(person => person.name).includes(newName)){
@@ -48,6 +69,13 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
+      <form>
+        <div>filter: <input value={newFilter} onChange={handleFilterChange}/></div>
+      </form>
+      
+
+      <h3>Add new</h3>
+
       <form onSubmit={addName}>
         <div>name: <input value={newName} onChange={handleChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumChange}/></div>
@@ -55,11 +83,11 @@ const App = () => {
           <button type="submit" >add</button>
         </div>
       </form>
-      
+
       <h2>Numbers</h2>
       <table>
           <tbody>
-            {persons.map(person =>           
+            {persons.filter(person => person.showing === true).map(person =>           
             <Number person={person} key={person.name} />        
             )}
           </tbody>

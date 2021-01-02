e0492b78f1e4998e5cfe5498014596fbba96eba7
Veera <veera.helli@gmail.com>
2021-01-02 17:11:30 +0200

    2.1 done

--- Osa2/kurssitiedot/src/index.js
+++ Osa2/kurssitiedot/src/index.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts}  />
+      <Total parts={props.course.parts} />
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = (props) => {
+  //console.log(props)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <tr>
+      <td>{props.part}</td>
+      <td>{props.exercises}</td>
+    </tr>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <table>
+        <tbody>
+            {props.parts.map(part =>
+            <Part part={part.name} exercises={part.exercises} />        
+            )}      
+        </tbody>
+    </table>
+  )
+}
+/*      <table>
+        <tbody>
+          <StatisticLine text="good" value ={good} />
+          <StatisticLine text="neutral" value ={neutral} />
+          <StatisticLine text="bad" value ={bad} />
+          <StatisticLine text="all" value ={all} />
+          <StatisticLine text="average" value ={(good-bad)/all} />
+          <StatisticLine text="positive" value ={(good/all)*100 + " %"} />
+        </tbody>
+      </table>    
+
+<div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+    */
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises</p>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Testing part',
+        exercises: 2,
+        id: 3
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 4
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Course course={course} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
